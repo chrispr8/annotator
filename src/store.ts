7667750868642fa7c6faa66dc36622355b89571a
Christoph Pracht <pracht@cl.uni-heidelberg.de>
2023-12-04 07:05:53 +0100

    Edited Venue type to be more generic

--- src/store.ts
+++ src/store.ts
@@ -3,54 +3,61 @@ import { derived, writable } from "svelte/store"
 
 import { localStorageStore } from "@skeletonlabs/skeleton"
 
+type Suburb = "Altstadt" | 
+              "Bahnstadt" | 
+              "Bergheim" | 
+              "Boxberg" | 
+              "Emmertsgrund" | 
+              "Handschuhsheim" | 
+              "Kirchheim" | 
+              "Neuenheim" | 
+              "Pfaffengrund" | 
+              "Rohrbach" |
+              "Schlierbach" |
+              "Südstadt" |
+              "Weststadt" |
+              "Wieblingen" | 
+              "Ziegelhausen"
+
+type Cuisine = "african" |
+               "american" |
+               "asian" |
+               "chinese" |
+               "german" |
+               "greek" |
+               "indian" |
+               "international" |
+               "italian" |
+               "japanese" |
+               "korean" |
+               "oriental" |
+               "sushi" |
+               "thai"
+
 // Declare venue type for data import and manipulation
 export type Venue = {
-    "type": "restaurant" | "hotel" | "attraction"
-    "name": string,
-    "description": string,
+    type: "restaurant" | "hotel" | "attraction"
+    name: string,
+    description: string,
     "addr:housenumber": string,
     "addr:street": string,
-    "addr:suburb": "Altstadt" | 
-                    "Bahnstadt" | 
-                    "Bergheim" | 
-                    "Boxberg" | 
-                    "Emmertsgrund" | 
-                    "Handschuhsheim" | 
-                    "Kirchheim" | 
-                    "Neuenheim" | 
-                    "Pfaffengrund" | 
-                    "Rohrbach" |
-                    "Schlierbach" |
-                    "Südstadt" |
-                    "Weststadt" |
-                    "Wieblingen" | 
-                    "Ziegelhausen",
-    "phone": string,
-    "website": string,
-    "cuisine": "african" |
-                "american" |
-                "asian" |
-                "chinese" |
-                "german" |
-                "greek" |
-                "indian" |
-                "international" |
-                "italian" |
-                "japanese" |
-                "korean" |
-                "oriental" |
-                "sushi" |
-                "thai", 
-    "delivery": "yes" | "no" | "unknown",
-    "indoor_seating": "yes" | "no" | "limited" | "unknown",
-    "internet_access": "yes" | "no" | "unknown",
-    "outdoor_seating": "yes" | "no" | "limited" | "unknown",
-    "price": "cheap" | "moderate" | "expensive" | "unknown",
-    "smoking": "yes" | "no" | "unknown",
-    "vegan": "yes" | "no" | "limited" | "unknown",
-    "vegetarian": "yes" | "no" | "limited" | "unknown",
-    "wheelchair": "yes" | "no" | "limited" | "unknown",
-    "edited": boolean,
+    "addr:suburb": Suburb,
+    phone: string,
+    website: string,
+    cuisine?: Cuisine[],
+    delivery?: "yes" | "no" | "unknown",
+    indoor_seating?: "yes" | "no" | "limited" | "unknown",
+    internet_access?: "yes" | "no" | "unknown",
+    outdoor_seating?: "yes" | "no" | "limited" | "unknown",
+    price: "cheap" | "moderate" | "expensive" | "free" | "unknown",
+    smoking?: "yes" | "no" | "unknown",
+    vegan?: "yes" | "no" | "limited" | "unknown",
+    vegetarian?: "yes" | "no" | "limited" | "unknown",
+    wheelchair?: "yes" | "no" | "limited" | "unknown",
+    stars?: "1" | "2" | "3" | "4" | "5" | "unknown",
+    air_conditioning?: "yes" | "no" | "unknown",
+    parking?: "yes" | "no" | "unknown",
+    edited: boolean,
 }
 
 export const dataStore: Writable<Array<Venue>> = localStorageStore("dataStore", [])
